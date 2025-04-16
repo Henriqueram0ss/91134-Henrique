23b821b2d5bdc5ca9446b785db6f2fb639a98119
Henriqueram0ss <>
2025-04-16 15:38:14 -0300

    Atividade JS

---
+++ Formulario/script.js
@@ -0,0 +1,32 @@
+function validateForm(){
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+
+
+    let errorMessage = document.getElementById('error-message');
+
+    errorMessage.textContent = '';
+
+    if (name === ''){
+        errorMessage.textContent = 'Por favor, insira seu nome.';
+        return false;
+    }
+
+    if (email === ''){
+        errorMessage.textContent = 'Por favor, insira seu email.';
+        return false;
+    }
+
+    if (password === ''){
+        errorMessage.textContent = 'Por favor, insira sua senha.';
+        return false;
+    }
+
+    if (password !==  confirmPassword){
+        errorMessage.textContent = ' As senhas não coincides.';
+        return false;
+    }
+    return true;
+}
